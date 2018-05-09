b60fd66aeb8349ae4ed8292769eb413eba8f2f93
Laurent VION <laurent.vion@m6.fr>
2018-05-09 17:04:49 +0200

    base

---
+++ baseif.py
@@ -0,0 +1,6 @@
+i = 13
+
+if i == 12:
+    print("i égal à 12 !")
+else:
+    print("i n'est pas égal à 12")
---
+++ basestruct.py
@@ -0,0 +1,13 @@
+#list
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi']
+
+c = len(fruits)
+
+print(c)
+
+#disctionary
+count = {'orange': 12, 'apple': 43}
+
+c = count['apple']
+
+print(c)
